b561f2e2541a8e0da4b779f11ac95826177e0cca
Jake Donham <jake@donham.org>
2008-08-27 02:09:13 +0000

    oops, library -> program. also, license for lwt_unix.ml[i]
    git-svn-id: http://orpc2.googlecode.com/svn/trunk@50 220da7d2-e253-0410-a747-191e32cba5d1

--- lwt-equeue/lwt_unix.ml
+++ lwt-equeue/lwt_unix.ml
@@ -1,24 +1,23 @@
-(* Lightweight thread library for Objective Caml
- * http://www.ocsigen.org/lwt
- * Module Lwt_unix
+(*
+ * This file is part of orpc, OCaml signature to ONC RPC generator
+ * Copyright (C) 2008 Skydeck, Inc
+ * Original file (lwt_unix.ml in the Lwt source distribution) is
  * Copyright (C) 2005-2008 Jérôme Vouillon
- * Laboratoire PPS - CNRS Université Paris Diderot
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, with linking exception;
- * either version 2.1 of the License, or (at your option) any later
- * version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
+ * 02111-1307, USA
  *)
 
 exception Event_system_not_set
