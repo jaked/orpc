06cc46b8f1fbccad41b136e8e0db4c9ec8251f7c
Jake Donham <jake@donham.org>
2009-05-19 00:55:23 +0000

    accept as an event. factored common code for sock op events
    git-svn-id: http://orpc2.googlecode.com/svn/trunk@110 220da7d2-e253-0410-a747-191e32cba5d1

--- src/lwt-equeue/lwt_event_unix.ml
+++ src/lwt-equeue/lwt_event_unix.ml
@@ -7,62 +7,61 @@ let sleep d =
     Lwt_event.sync (Lwt_event.send chan ()) in
   Lwt_event.receive chan
 
-let read ch buf pos len =
+let sockop watchers ch name f =
   Lwt_event.behavior (fun performed condition evnum ->
     let res = ref `None in
     let fd = Lwt_unix.unix_file_descr ch in
+
+    let try_op () =
+      let e =
+        try Lwt_unix.check_descriptor ch; None
+        with e -> Some e in
+      begin res :=
+        match e with
+          | None -> `Value (f fd) (* may raise EAGAIN / EWOULDBLOCK from f *)
+          | Some e -> `Exn e
+      end;
+      performed := evnum in
+
     let poll () =
-      try
-        Lwt_unix.check_descriptor ch;
-        try
-          res := `Value (Unix.read fd buf pos len);
-          performed := evnum;
-          true
-        with
-          | Unix.Unix_error ((Unix.EAGAIN | Unix.EWOULDBLOCK), _, _) -> false
-      with e ->
-        res := `Exn e;
-        true in
+      try try_op (); true
+      with Unix.Unix_error ((Unix.EAGAIN | Unix.EWOULDBLOCK), _, _) -> false in
+
     let suspend () =
-      ignore (Lwt_unix.register_action Lwt_unix.inputs ch (fun () ->
+      ignore (Lwt_unix.register_action watchers ch (fun () ->
         if !performed < 0
-        then
-          if poll ()
-          then Lwt.wakeup condition ()
-          else raise Lwt_unix.Retry_read)) in
+        then begin
+          try_op (); (* EAGAIN / EWOULDBLOCK caught by Lwt_unix.wrap_syscall *)
+          Lwt.wakeup condition ()
+        end)) in
+
     let result () =
       match !res with
-        | `None -> invalid_arg "Lwt_event_unix.read"
+        | `None -> invalid_arg name
         | `Value v -> v
         | `Exn e -> raise e in
+
     { Lwt_event.poll = poll; suspend = suspend; result = result })
 
+let read ch buf pos len =
+  sockop
+    Lwt_unix.inputs
+    ch
+    "Lwt_event_unix.read"
+    (fun fd -> Unix.read fd buf pos len)
+
 let write ch buf pos len =
-  Lwt_event.behavior (fun performed condition evnum ->
-    let res = ref `None in
-    let fd = Lwt_unix.unix_file_descr ch in
-    let poll () =
-      try
-        Lwt_unix.check_descriptor ch;
-        try
-          res := `Value (Unix.write fd buf pos len);
-          performed := evnum;
-          true
-        with
-          | Unix.Unix_error ((Unix.EAGAIN | Unix.EWOULDBLOCK), _, _) -> false
-      with e ->
-        res := `Exn e;
-        true in
-    let suspend () =
-      ignore (Lwt_unix.register_action Lwt_unix.outputs ch (fun () ->
-        if !performed < 0
-        then
-          if poll ()
-          then Lwt.wakeup condition ()
-          else raise Lwt_unix.Retry_write)) in
-    let result () =
-      match !res with
-        | `None -> invalid_arg "Lwt_event_unix.write"
-        | `Value v -> v
-        | `Exn e -> raise e in
-    { Lwt_event.poll = poll; suspend = suspend; result = result })
+  sockop
+    Lwt_unix.outputs
+    ch
+    "Lwt_event_unix.write"
+    (fun fd -> Unix.write fd buf pos len)
+
+let accept ch =
+  sockop
+    Lwt_unix.inputs
+    ch
+    "Lwt_event_unix.accept"
+    (fun fd ->
+      let (s, addr) = Unix.accept fd in
+      (Lwt_unix.of_unix_file_descr s, addr))
