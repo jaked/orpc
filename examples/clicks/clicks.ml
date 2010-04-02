2e0a1427077814f6ff47ec851d65524515429b21
Jake Donham <jake@donham.org>
2010-04-02 15:29:29 -0400

    Revert "fix for trunk ocamljs"
    This reverts commit 3a47450de6e2ad009592388249045b0eee866f46.

--- examples/clicks/clicks.ml
+++ examples/clicks/clicks.ml
@@ -5,14 +5,14 @@ let (>>=) = Lwt.(>>=)
 
 ;;
 
-Dom.window#_set_onload (fun () ->
+Dom.window#_set_onload (Ocamljs.jsfun (fun () ->
   let clicks = (Dom.document#getElementById "clicks" : Dom.span) in
   let click = (Dom.document#getElementById "click" : Dom.button) in
 
   let set_clicks n = Lwt.return (clicks#_set_innerHTML (string_of_int n)) in
 
-  click#_set_onclick (fun _ ->
+  click#_set_onclick (Ocamljs.jsfun (fun _ ->
     ignore(Server.click () >>= set_clicks);
-    Ocamljs.false_ ());
+    Ocamljs.false_ ()));
 
-  ignore(Server.clicks () >>= set_clicks))
+  ignore(Server.clicks () >>= set_clicks)))
