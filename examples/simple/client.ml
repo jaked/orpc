a72c0e203ca7d4e41c29da754531e7b71ce4d56e
Jake Donham <jake@donham.org>
2010-04-01 20:32:59 -0400

    combined simple_async into simple

--- examples/simple/client.ml
+++ examples/simple/client.ml
@@ -5,35 +5,74 @@ let rec lst_of_list l =
 
 module Pp = Protocol_trace.Pp
 
+let main () =
+  let c = Protocol_clnt.create_client (Rpc_client.Inet ("localhost", 9007)) Rpc.Tcp in
+  let fmt = Format.err_formatter in
+
+  Pp.orpc_trace_pp_add1'call fmt 6;
+  Pp.orpc_trace_pp_add1'reply fmt
+    (Protocol_clnt.add1 c 6);
+  
+  Pp.orpc_trace_pp_addN'call fmt ~n:7 6;
+  Pp.orpc_trace_pp_addN'reply fmt
+    (Protocol_clnt.addN c ~n:7 6);
+  
+  Pp.orpc_trace_pp_add1_list'call fmt [5;6;7];
+  Pp.orpc_trace_pp_add1_list'reply fmt
+    (Protocol_clnt.add1_list c [5;6;7]);
+  
+  Pp.orpc_trace_pp_add1_lst'call fmt (lst_of_list [7;8;9]);
+  Pp.orpc_trace_pp_add1_lst'reply fmt
+    (Protocol_clnt.add1_lst c (lst_of_list [7;8;9]));
+  
+  Pp.orpc_trace_pp_add1_pair'call fmt (17, 22);
+  Pp.orpc_trace_pp_add1_pair'reply fmt
+    (Protocol_clnt.add1_pair c (17, 22));
+  
+  Pp.orpc_trace_pp_maybe_raise'call fmt true;
+  try
+    Pp.orpc_trace_pp_maybe_raise'reply fmt
+      (Protocol_clnt.maybe_raise c true)
+  with e -> Pp.orpc_trace_pp_exn'reply fmt e;
+  
+  Rpc_client.shut_down c
+
+let main_async () =
+  let esys = Unixqueue.create_unix_event_system() in
+  let c = Protocol_clnt.create_client ~esys (Rpc_client.Inet ("localhost", 9007)) Rpc.Tcp in
+  let fmt = Format.err_formatter in
+  
+  Pp.orpc_trace_pp_add1'call fmt 6;
+  Protocol_clnt.add1'async c 6
+    (fun r -> Pp.orpc_trace_pp_add1'reply fmt (r ()));
+  
+  Pp.orpc_trace_pp_addN'call fmt ~n:7 6;
+  Protocol_clnt.addN'async c ~n:7 6
+    (fun r -> Pp.orpc_trace_pp_addN'reply fmt (r ()));
+  
+  Pp.orpc_trace_pp_add1_list'call fmt [5;6;7];
+  Protocol_clnt.add1_list'async c [5;6;7]
+    (fun r -> Pp.orpc_trace_pp_add1_list'reply fmt (r ()));
+  
+  Pp.orpc_trace_pp_add1_lst'call fmt (lst_of_list [7;8;9]);
+  Protocol_clnt.add1_lst'async c (lst_of_list [7;8;9])
+    (fun r -> Pp.orpc_trace_pp_add1_lst'reply fmt (r ()));
+  
+  Pp.orpc_trace_pp_add1_pair'call fmt (17, 22);
+  Protocol_clnt.add1_pair'async c (17, 22)
+    (fun r -> Pp.orpc_trace_pp_add1_pair'reply fmt (r ()));
+  
+  Pp.orpc_trace_pp_maybe_raise'call fmt true;
+  Protocol_clnt.maybe_raise'async c true
+    (fun r ->
+      try Pp.orpc_trace_pp_maybe_raise'reply fmt (r ())
+      with e -> Pp.orpc_trace_pp_exn'reply fmt e);
+  
+  Unixqueue.run esys;
+  Rpc_client.shut_down c
+
 ;;
 
-let c = Protocol_clnt.create_client (Rpc_client.Inet ("localhost", 9007)) Rpc.Tcp in
-let fmt = Format.err_formatter in
+main ()
+(* main_async () *)
 
-Pp.orpc_trace_pp_add1'call fmt 6;
-Pp.orpc_trace_pp_add1'reply fmt
-  (Protocol_clnt.add1 c 6);
-
-Pp.orpc_trace_pp_addN'call fmt ~n:7 6;
-Pp.orpc_trace_pp_addN'reply fmt
-  (Protocol_clnt.addN c ~n:7 6);
-
-Pp.orpc_trace_pp_add1_list'call fmt [5;6;7];
-Pp.orpc_trace_pp_add1_list'reply fmt
-  (Protocol_clnt.add1_list c [5;6;7]);
-
-Pp.orpc_trace_pp_add1_lst'call fmt (lst_of_list [7;8;9]);
-Pp.orpc_trace_pp_add1_lst'reply fmt
-  (Protocol_clnt.add1_lst c (lst_of_list [7;8;9]));
-
-Pp.orpc_trace_pp_add1_pair'call fmt (17, 22);
-Pp.orpc_trace_pp_add1_pair'reply fmt
-  (Protocol_clnt.add1_pair c (17, 22));
-
-Pp.orpc_trace_pp_maybe_raise'call fmt true;
-try
-  Pp.orpc_trace_pp_maybe_raise'reply fmt
-    (Protocol_clnt.maybe_raise c true)
-with e -> Pp.orpc_trace_pp_exn'reply fmt e;
-
-Rpc_client.shut_down c
--- examples/simple/server.ml
+++ examples/simple/server.ml
@@ -1,29 +1,70 @@
-let proc_add1 i = i + 1
+let rec lst_map f l =
+  match l with
+    | Protocol_aux.Nil -> Protocol_aux.Nil
+    | Protocol_aux.Cons (a,l) -> Protocol_aux.Cons (f a, lst_map f l)
 
-let proc_add1_list l = List.map (fun i -> i + 1) l
+module Sync =
+struct
+  let proc_add1 i = i + 1
+  
+  let proc_add1_list l = List.map (fun i -> i + 1) l
+  
+  let proc_add1_pair (a, b) = (a + 1, b + 1)
+  
+  let proc_add1_r { Protocol_aux.fst = f; snd = s; trd = t } =
+    {
+      Protocol_aux.fst = f + 1;
+      snd = (match s with None -> None | Some s -> Some (s + 1));
+      trd = Array.map (fun e -> e + 1) t;
+    }
+  
+  let proc_add1_lst l = lst_map (fun i -> i + 1) l
+  
+  let proc_addN ?(n = 1) i = i + n
+  
+  let proc_maybe_raise flag =
+    if flag
+    then raise (Protocol_aux.Bar 17)
 
-let proc_add1_pair (a, b) = (a + 1, b + 1)
+  let setup srv = 
+    Protocol_srv.bind
+      ~proc_add1 ~proc_add1_list ~proc_add1_pair
+      ~proc_add1_r ~proc_add1_lst ~proc_addN
+      ~proc_maybe_raise
+      srv
+end
 
-let proc_add1_r { Protocol_aux.fst = f; snd = s; trd = t } =
-  {
-    Protocol_aux.fst = f + 1;
-    snd = (match s with None -> None | Some s -> Some (s + 1));
-    trd = Array.map (fun e -> e + 1) t;
-  }
+module Async =
+struct
+  let proc_add1 _ i r = r (i + 1)
+  
+  let proc_add1_list _ l r = r (List.map (fun i -> i + 1) l)
+  
+  let proc_add1_pair _ (a, b) r = r (a + 1, b + 1)
+  
+  let proc_add1_r _ { Protocol_aux.fst = f; snd = s; trd = t } r =
+    r {
+      Protocol_aux.fst = f + 1;
+      snd = (match s with None -> None | Some s -> Some (s + 1));
+      trd = Array.map (fun e -> e + 1) t;
+    }
+  
+  let proc_add1_lst _ l r = r (lst_map (fun i -> i + 1) l)
+  
+  let proc_addN _ ?(n = 1) i r = r (i + n)
+  
+  let proc_maybe_raise _ flag r =
+    if flag
+    then raise (Protocol_aux.Bar 17)
+    else r ()
 
-let proc_add1_lst l =
-  let rec lst_map f l =
-    match l with
-      | Protocol_aux.Nil -> Protocol_aux.Nil
-      | Protocol_aux.Cons (a,l) -> Protocol_aux.Cons (f a, lst_map f l)
-  in
-  lst_map (fun i -> i + 1) l
-
-let proc_addN ?(n = 1) i = i + n
-
-let proc_maybe_raise flag =
-  if flag
-  then raise (Protocol_aux.Bar 17)
+  let setup srv = 
+    Protocol_srv.bind_async
+      ~proc_add1 ~proc_add1_list ~proc_add1_pair
+      ~proc_add1_r ~proc_add1_lst ~proc_addN
+      ~proc_maybe_raise
+      srv
+end
 
 let start() =
   let (opt_list, cmdline_cfg) = Netplex_main.args() in
@@ -38,11 +79,8 @@ let start() =
         ~configure:(fun _ _ -> ())
         ~name:"add1"
         ~setup:(fun srv () ->
-          Protocol_srv.bind
-            ~proc_add1 ~proc_add1_list ~proc_add1_pair
-            ~proc_add1_r ~proc_add1_lst ~proc_addN
-            ~proc_maybe_raise
-            srv)
+                  Sync.setup srv
+                  (* Async.setup srv *))
         ();
     ]
   in
