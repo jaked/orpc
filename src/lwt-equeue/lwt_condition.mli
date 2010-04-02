d032557ba17e6664d6e87211486d8c16f1263784
Jake Donham <jake@donham.org>
2010-04-02 18:27:09 -0400

    updated copyright

--- src/lwt-equeue/lwt_condition.mli
+++ src/lwt-equeue/lwt_condition.mli
@@ -1,6 +1,7 @@
 (*
  * This file is part of orpc, OCaml signature to ONC RPC generator
  * Copyright (C) 2008-9 Skydeck, Inc
+ * Copyright (C) 2010 Jacob Donham
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
